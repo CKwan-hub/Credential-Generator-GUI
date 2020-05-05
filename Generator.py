27144099411d082a101139879e7e6bd1e14dd626
CKwan-Hub <ckwandrans@gmail.com>
2020-05-05 18:39:30 -0400

    cleaned up debugging - password options and minor cleanup remaining

--- Generator.py
+++ Generator.py
@@ -1,7 +1,12 @@
 
 
 # TODO:
-
+#
+# Comment on all new functions
+# Rename combobox options
+# Complete info box on main frame
+# additional styles or fonts?
+#
 # Strong vs Realistic password generation
 # In main repo, create a function with random selection
 # if hits "strong" vs the normal selection route,
@@ -47,7 +52,6 @@ secondFrame = tkinter.Frame(width=180, height=170,
                             borderwidth=2, relief='groove')
 secondFrame.grid(row=0, column=1)
 secondFrame.grid_propagate(0)
-# secondFrame.grid_rowconfigure((0, 12), weight=1)
 secondFrame.grid_columnconfigure((0, 1), weight=1)
 
 # Run Button Frame.
@@ -55,7 +59,6 @@ runFrame = tkinter.Frame(
     width=180, height=60, borderwidth=2, relief='groove')
 runFrame.grid(row=1, column=1)
 runFrame.grid_propagate(0)
-# runFrame.grid_rowconfigure((0, 2), weight=1)
 runFrame.grid_columnconfigure((0, 1), weight=1)
 
 url = ''
@@ -65,7 +68,6 @@ password = ''
 lengthOptionsArray = ['short_text.json', 'medium_text.json',
                       'long_text.json', 'longest_text.json', 'million_text.json']
 
-# print(lengthOptionsArray[1])
 
 # string of ascii letters in both upppercase & lowercase + string of digits  + spec characters
 chars = string.ascii_letters + string.digits + '!?@#$%&*'
@@ -93,7 +95,6 @@ passValue.set('')
 
 comboTxt = tkinter.StringVar()
 comboTxt.set('M')
-print('comboTxt', comboTxt.get())
 
 lengthOptions = ttk.Combobox(secondFrame, values=[
     "S",
@@ -102,14 +103,6 @@ lengthOptions = ttk.Combobox(secondFrame, values=[
     "XL",
     "MILLION"
 ], state="readonly", font=dropdownFont, textvariable=comboTxt).grid(row=2, columnspan=2, pady=2)
-# ^ before ".grid", add font if desired
-
-# print(lengthOptions)
-
-
-# testBtn = tkinter.Button(runFrame, text='X', command=setLength)
-# testBtn.grid(row=1, columnspan=2)
-# setLength()
 
 
 def mainFunction():
@@ -119,32 +112,20 @@ def mainFunction():
 
     seedFile = tkinter.StringVar()
     seedFile.set('medium_text.json')
-    print('seedFile', seedFile.get())
 
-    def setLength():
-        print('pressed')
-        print('comboTxt', comboTxt.get())
-        print('seedfile:', seedFile.get())
+
+f
+   def setLength():
         if (comboTxt.get() == 'S'):
-            print('matchedS')
             seedFile.set('short_text.json')
-            print(seedFile.get())
         if (comboTxt.get() == 'M'):
-            print('matchedM')
             seedFile.set('medium_text.json')
-            print(seedFile.get())
         if (comboTxt.get() == 'L'):
-            print('matchedL')
             seedFile.set('long_text.json')
-            print(seedFile.get())
         if (comboTxt.get() == 'XL'):
-            print('matchedXL')
             seedFile.set('longest_text.json')
-            print(seedFile.get())
         if (comboTxt.get() == 'MILLION'):
-            print('matchedMil')
             seedFile.set('million_text.json')
-            print(seedFile.get())
 
     setLength()
 
@@ -153,10 +134,6 @@ def mainFunction():
 
     tkinter.Label(runFrame, text="Generating UserData...", fg='red').grid(
         row=1, columnspan=2, pady=2)
-    print('running main')
-
-    # def lengthSelection(event)
-    #
 
     for email_data in email_text:
 
@@ -177,21 +154,12 @@ def mainFunction():
 
         def sendURL():
             if (chkVal2.get() == 1):
-                print('url', urlValue.get())
-                print('userValue', userValue.get())
-                print('passValue', passValue.get())
                 requests.post((urlValue.get()), allow_redirects=False, data={
                     userValue.get(): username,
                     passValue.get(): password
                 })
 
         sendURL()
-        # Post to the specified url, don't redirect and pass usernames and passwords.
-        # !!Need to specify the var for username and password from submission form.
-        # requests.post(url, allow_redirects=False, data={
-        #     '#': username,
-        #     '#': password
-        # })
 
         # Output list of usernames and passwords.
         if (chkVal.get() == 1):
@@ -201,27 +169,15 @@ def mainFunction():
             print('Username: %s Password: %s' % (username, password))
 
 
-# label_widget = tk.Label(widget, option=placeholder)
-# checkbutton_widget = tk.CheckButton(widget, option=placeholder)
-
 # open output.txt in append mode.
 output_file = open('output.txt', 'a')
 
 
 def outputCheck():
-    # global username
-    # global password
-    # global output
     if (chkVal.get() == 1):
-        print('checked')
         outputText.set('Data Generated In \"Output.txt\"')
-        # # Output list of usernames and passwords.
-        # output = output_file.write('\"Username:\" \'% s\' \"Password:\" \'% s\' \n' %
-        #                            (username, password))
     if (chkVal.get() == 0):
-        print('unchecked')
         outputText.set('')
-        # print('Username: %s Password: %s' % (username, password))
 
 
 descInfoTxt = tkinter.StringVar()
@@ -236,10 +192,6 @@ tkinter.Label(secondFrame).grid(row=0, columnspan=2)
 tkinter.Label(secondFrame, text="Select Results Length:").grid(
     row=1, columnspan=2, pady=1)
 
-# comboTxt = tkinter.StringVar()
-# comboTxt.set('M - 1,750 Results')
-# print(comboTxt.get())
-
 
 # passwordComplex =
 chkShow = tkinter.IntVar()
@@ -249,10 +201,8 @@ passwordText.set("")
 
 def showWarn():
     if (chkShow.get() == 1):
-        print('checked')
         passwordText.set("Generating realistic passwords.")
     if (chkShow.get() == 0):
-        print('unchecked')
         passwordText.set("")
 
 
@@ -263,10 +213,6 @@ chkVal = tkinter.IntVar()
 tkinter.Checkbutton(
     secondFrame, text="Output To Text File", variable=chkVal, onvalue=1, offvalue=0, command=outputCheck).grid(row=4, columnspan=2, pady=1)
 
-
-# tkinter.Label(secondFrame, pady=1).grid(row=5, columnspan=2)
-# Wire up function on length selection.
-# lengthOptions.bind("<<ComboboxSelected>>", lengthSelection)
 
 outputText = tkinter.StringVar()
 tkinter.Label(
@@ -295,7 +241,6 @@ passwordEntry.grid(row=7, column=1, pady=3)
 def enableEntry():
     global urlLabel, urlEntry, emailLabel, emailEntry, passwordLabel, passwordEntry, urlValue, userValue, passValue
     if (chkVal2.get() == 1):
-        print('checked')
         urlLabel = tkinter.Label(
             parentFrame, text='Target URL', state='normal')
         urlLabel.grid(row=5, pady=3)
@@ -315,10 +260,6 @@ def enableEntry():
             parentFrame, textvariable=passValue, state='normal')
         passwordEntry.grid(row=7, column=1, pady=3)
     if (chkVal2.get() == 0):
-        print('unchecked')
-        print(urlValue.get())
-        print(userValue.get())
-        print(passValue.get())
         urlLabel = tkinter.Label(
             parentFrame, text='Target URL', state='disabled')
         urlLabel.grid(row=5, pady=3)
@@ -352,19 +293,9 @@ tkinter.Label(secondFrame, textvariable=passwordText, fg='red').grid(
     row=6, columnspan=2, pady=1)
 
 
-def runGenerator():
-
-    # tkinter.Label(parentFrame, text="Generating UserData...").grid(
-    #     row=11, columnspan=2)
-    print('running main')
-
-
 btn1 = tkinter.Button(runFrame, text="Generate",  command=mainFunction)
 btn1.grid(
     row=0, columnspan=2, pady=3)
-# btn1.bind(mainFunction)
-
-# tkinter.Label(parentFrame).grid(row=11)
 
 
 window.mainloop()
