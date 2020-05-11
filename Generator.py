2cdfa585457b7ac50403a586ea064d9b8dcb2335
CKwan-Hub <ckwandrans@gmail.com>
2020-05-11 00:21:48 -0400

    Finished commenting - Cleaned up - Added potential new feature ideas

--- Generator.py
+++ Generator.py
@@ -2,18 +2,14 @@
 
 # TODO:
 #
-# Comment on all new functions
 # Rename combobox options
-# Complete info box on main frame
 # additional styles or fonts?
+# Icon
 #
-# Strong vs Realistic password generation
-# In main repo, create a function with random selection
-# if hits "strong" vs the normal selection route,
-# do random.choice(word from seed), plus 1-4 num/symbol
-#
-#
-#
+# Stagger generation/submission by X (input) time in seconds for URL submissions?
+# Open readme button
+# Integrated terminal window?
+# Hide/show terminal toggle?
 
 # Importing.
 import tkinter
@@ -61,10 +57,12 @@ runFrame.grid(row=1, column=1)
 runFrame.grid_propagate(0)
 runFrame.grid_columnconfigure((0, 1), weight=1)
 
+# Setting input variables.
 url = ''
 username = ''
 password = ''
 
+# Options for seed files.
 lengthOptionsArray = ['short_text.json', 'medium_text.json',
                       'long_text.json', 'longest_text.json', 'million_text.json']
 
@@ -85,6 +83,7 @@ password_length = [6, 7, 8, 9, 10, 11, 12]
 # choices for generated extra email numbers length.
 extra_length = [0, 1, 2, 3, 4]
 
+# Setting User Input Values.
 urlValue = tkinter.StringVar()
 urlValue.set('')
 userValue = tkinter.StringVar()
@@ -92,10 +91,11 @@ userValue.set('')
 passValue = tkinter.StringVar()
 passValue.set('')
 
-
+# Setting ComboBox Default.
 comboTxt = tkinter.StringVar()
 comboTxt.set('M')
 
+# Combobox object for output length/seedfile selection.
 lengthOptions = ttk.Combobox(secondFrame, values=[
     "S",
     "M",
@@ -104,25 +104,31 @@ lengthOptions = ttk.Combobox(secondFrame, values=[
     "MILLION"
 ], state="readonly", font=dropdownFont, textvariable=comboTxt).grid(row=2, columnspan=2, pady=2)
 
+# Setting Password ComboBox Default.
 passCmbTxt = tkinter.StringVar()
 passCmbTxt.set('Randomized')
 
+# Combobox object for password type selection.
 passOptions = ttk.Combobox(secondFrame, values=[
     "Realistic",
     "Strong",
     "Ramdomized"
 ], state="readonly", font=dropdownFont, textvariable=passCmbTxt).grid(row=4, columnspan=2, pady=1)
 
+# Main function for generation.
+
 
 def mainFunction():
-
+    # Importing globals for input.
     global urlValue
     global userValue
     global passValue
 
+    # Default seed file/.json length.
     seedFile = tkinter.StringVar()
     seedFile.set('medium_text.json')
 
+    # Get values from the length combobox and select the .json file associated.
     def setLength():
         if (comboTxt.get() == 'S'):
             seedFile.set('short_text.json')
@@ -140,7 +146,8 @@ def mainFunction():
     # list of text to act as the email base value.
     email_text = json.loads(open(seedFile.get()).read())
 
-    tkinter.Label(runFrame, text="Generating UserData...", fg='red').grid(
+    # User message for completion.
+    tkinter.Label(runFrame, text="Data Generated!", fg='red').grid(
         row=1, columnspan=2, pady=2)
 
     for email_data in email_text:
@@ -154,28 +161,30 @@ def mainFunction():
         # choices for generated password additions
         pass_length = [1, 2, 3]
 
+        # Add random selection of characters (at random length) to realistic password generation
         pass_digits = ''.join(random.choice(chars)
                               for i in range(random.choice(pass_length)))
 
-        # Strong password choice.
+        # Strong password choice. (Random selection of characters at a random selection of lengths)
         strongPass = ''.join(random.choice(chars)
                              for i in range(random.choice(password_length)))
 
-        # Realistic password generation.
+        # Realistic password generation. (Random Word +/- Random Word + random digits/chars at random selection of lengths)
         realPass = ''.join(random.choice(email_text) +
                            random.choice(name_random) + pass_digits)
 
-        # Realistic password chance.
+        # Options for randomized password selection.
         passSelect = [strongPass, realPass, realPass]
 
         # lowercase values from email_text + random digits + random choice of email suffix.
         username = email_data.lower() + random.choice(name_random) + \
             name_digits + random.choice(email_list)
 
+        # Initial value for password default behavior.
         passwordVal = tkinter.StringVar()
         passwordVal.set(random.choice(passSelect))
-        # random selection of upper & lower case characters/digits/special characters at a length of 6-12.
 
+        # Combobox association for desired password type selection.
         def passStyle():
             if (passCmbTxt.get() == 'Randomized'):
                 passwordVal.set(random.choice(passSelect))
@@ -185,8 +194,11 @@ def mainFunction():
                 passwordVal.set(strongPass)
 
         passStyle()
+
+        # Retrieve selection of password output type.
         password = passwordVal.get()
 
+        # Check for if sending to external URL is selected.
         def sendURL():
             if (chkVal2.get() == 1):
                 requests.post((urlValue.get()), allow_redirects=False, data={
@@ -196,7 +208,7 @@ def mainFunction():
 
         sendURL()
 
-        # Output list of usernames and passwords.
+        # Output list of usernames and passwords to external if checked, if not, print values.
         if (chkVal.get() == 1):
             output_file.write('\"Username:\" \'% s\' \"Password:\" \'% s\' \n' %
                               (username, password))
@@ -208,6 +220,24 @@ def mainFunction():
 output_file = open('output.txt', 'a')
 
 
+# Modal description info.
+descInfoTxt = tkinter.StringVar()
+descInfoTxt.set(
+    'Select Output Preference\n Choose Desired Output Length \n Select Password Style \n Run Generator')
+descInfo = tkinter.Label(parentFrame, textvariable=descInfoTxt)
+descInfo.grid(row=0, columnspan=2, pady=2)
+
+# Info for Length Combobox.
+tkinter.Label(secondFrame, text="Select Results Length:").grid(
+    row=1, columnspan=2, pady=(11, 1))
+
+# Info for Password style Combobox.
+tkinter.Label(secondFrame, text="Select Password Types:").grid(
+    row=3, columnspan=2, pady=1)
+
+# Toggle message showing where output can be found.
+
+
 def outputCheck():
     if (chkVal.get() == 1):
         outputText.set('Data Generated In \"Output.txt\"')
@@ -215,47 +245,18 @@ def outputCheck():
         outputText.set('')
 
 
-descInfoTxt = tkinter.StringVar()
-descInfoTxt.set(
-    'Select Output Preference\n Choose Desired Output Length \n Select Password Style \n Run Generator')
-descInfo = tkinter.Label(parentFrame, textvariable=descInfoTxt)
-descInfo.grid(row=0, columnspan=2, pady=2)
-
-
-# tkinter.Label(secondFrame).grid(row=0, columnspan=2)
-
-tkinter.Label(secondFrame, text="Select Results Length:").grid(
-    row=1, columnspan=2, pady=(11, 1))
-
-
-# passwordComplex =
-# chkShow = tkinter.IntVar()
-# passwordText = tkinter.StringVar()
-# passwordText.set("")
-
-
-# def showWarn():
-#     if (chkShow.get() == 1):
-#         passwordText.set("Generating realistic passwords.")
-#     if (chkShow.get() == 0):
-#         passwordText.set("")
-
-
-tkinter.Label(secondFrame, text="Select Password Types:").grid(
-    row=3, columnspan=2, pady=1)
-# tkinter.Checkbutton(
-#     secondFrame, text="Realistic Passwords", command=showWarn, variable=chkShow, onvalue=1, offvalue=0).grid(row=3, columnspan=2, pady=1)
-
+# Checkbutton for external output.
 chkVal = tkinter.IntVar()
 tkinter.Checkbutton(
     secondFrame, text="Output To Text File", variable=chkVal, onvalue=1, offvalue=0, command=outputCheck).grid(row=5, columnspan=2, pady=1)
 
-
+# Info message for external output.
 outputText = tkinter.StringVar()
 tkinter.Label(
     secondFrame, textvariable=outputText, fg='red').grid(row=6, columnspan=2, pady=1)
 outputText.set('')
 
+# Initial values and disabled state for URL/Email/Password input fields.
 urlLabel = tkinter.Label(
     parentFrame, text='Target URL', state='disabled')
 urlLabel.grid(row=5, pady=3)
@@ -273,6 +274,8 @@ passwordLabel.grid(row=7, pady=3)
 passwordEntry = tkinter.Entry(
     parentFrame, textvariable=passValue, state='disabled')
 passwordEntry.grid(row=7, column=1, pady=3)
+
+# Function for toggling and allowing input of URL/Email/Password fields. Disabled state on unchecked.
 
 
 def enableEntry():
@@ -317,19 +320,16 @@ def enableEntry():
         passwordEntry.grid(row=7, column=1, pady=3)
 
 
+# Formatting.
 tkinter.ttk.Separator(parentFrame, orient="horizontal").grid(
     row=3, sticky='ew', columnspan=2)
 
-
+# Checkbutton allowing editing of URL/Email/Password fields.
 chkVal2 = tkinter.IntVar()
 tkinter.Checkbutton(
     parentFrame, text="Send to URL", command=enableEntry, variable=chkVal2, onvalue=1, offvalue=0, pady=5).grid(row=4, columnspan=2)
 
-#
-# tkinter.Label(secondFrame, textvariable=passwordText, fg='red').grid(
-#     row=6, columnspan=2, pady=1)
-
-
+# Generation Button/
 btn1 = tkinter.Button(runFrame, text="Generate",  command=mainFunction)
 btn1.grid(
     row=0, columnspan=2, pady=3)
